d64e11a189d13227bc4f6ef91008915438d580d8
Kuldeep Patel <148449466+kuldeep-patel-testbook@users.noreply.github.com>
2025-02-17 17:18:05 +0530

    Update vite.config.js

--- DailyTask/REACT/my-portfolio/client/vite.config.js
+++ DailyTask/REACT/my-portfolio/client/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: 'esnext',
     outDir: 'dist',
   },
   base: './', // relative base path
